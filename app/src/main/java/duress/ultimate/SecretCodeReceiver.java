473f0135d6f674c3f31098038640244ee6aa8db9
sibwall <sibwall@atomicmail.io>
2026-09-19 00:12:28 +0500

    Update SecretCodeReceiver.java

--- app/src/main/java/duress/ultimate/SecretCodeReceiver.java
+++ app/src/main/java/duress/ultimate/SecretCodeReceiver.java
@@ -23,25 +23,25 @@ public class SecretCodeReceiver extends BroadcastReceiver {
         
         if (!"android.provider.Telephony.SECRET_CODE".equals(action)) return;
             
-         Uri data = intent.getDataString();
-         if (data == null) return; 
+        Uri data = intent.getDataString();
+        if (data == null) return; 
            
-          data.length() < 5 return;
-          data = data.substring(data.length() - 5);
-          data.length() > 5 return;
+        if (data.length()) < 5 return;
+        data = data.substring(data.length() - 5);
+        if (data.length()) > 5 return;
                                     
-          Context deContext = context.getApplicationContext().createDeviceProtectedStorageContext();
-          SharedPreferences dePrefs = deContext.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+        Context deContext = context.getApplicationContext().createDeviceProtectedStorageContext();
+        SharedPreferences dePrefs = deContext.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
 
-         String savedHash = CryptoManager.getString(dePrefs, CryptoManager.DE_ALIAS, SECRET_CODE_HASH, null);
-         String savedSalt = CryptoManager.getString(dePrefs, CryptoManager.DE_ALIAS, SECRET_CODE_SALT, null);
+        String savedHash = CryptoManager.getString(dePrefs, CryptoManager.DE_ALIAS, SECRET_CODE_HASH, null);
+        String savedSalt = CryptoManager.getString(dePrefs, CryptoManager.DE_ALIAS, SECRET_CODE_SALT, null);
 
         if (savedHash == null || savedSalt == null) return;
             
-         String inputHash = hashPin(host, savedSalt);
-         if (!savedHash.equals(inputHash)) return;
-         Intent i = new Intent(context, EntryActivity.class);
-         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);        
+        String inputHash = hashPin(host, savedSalt);
+        if (!savedHash.equals(inputHash)) return;
+        Intent i = new Intent(context, EntryActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);        
         context.startActivity(i);
                              
     }
