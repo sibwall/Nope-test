c2e45bbfdce4b994e0327a14d1fdea22193aa4c9
sibwall <sibwall@atomicmail.io>
2026-09-19 20:02:13 +0500

    Update CalculatorActivity.java

--- app/src/main/java/duress/ultimate/CalculatorActivity.java
+++ app/src/main/java/duress/ultimate/CalculatorActivity.java
@@ -14,7 +14,6 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
